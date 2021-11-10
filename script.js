dd99aca72c0ac0e4c1bd3665a7268bd1f37280e9
Alina Shilo <alinashilo98@gmail.com>
2021-11-10 19:04:27 +0300

    feat: implement countdown

---
+++ script.js
@@ -0,0 +1,25 @@
+let currentYear = new Date().getFullYear();
+const nextYear = new Date(currentYear + 1, 0);
+
+const daysOutput = document.querySelector('#days');
+const hoursOutput = document.querySelector('#hours');
+const minutesOutput = document.querySelector('#minutes');
+const secondsOutput = document.querySelector('#seconds');
+
+function countdown() {
+  setInterval(() => {
+    let currentTime = new Date();
+    let distance = nextYear - currentTime;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(distance / (1000 * 60 * 60) % 24);
+    let minutes = Math.floor(distance / (1000 * 60) % 60);
+    let seconds = Math.floor(distance / 1000 % 60);
+    daysOutput.textContent = `${days}`;
+    hoursOutput.textContent = `${hours}`;
+    minutesOutput.textContent = `${minutes}`;
+    secondsOutput.textContent = `${seconds}`;
+  }, 1000);
+}
+
+countdown();
