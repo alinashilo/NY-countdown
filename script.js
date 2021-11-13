522846853871dcb40721f1ec12c311d655e94172
Alina Shilo <alinashilo98@gmail.com>
2021-11-14 02:58:29 +0300

    fix: remove delay at the start of the function

--- script.js
+++ script.js
@@ -7,19 +7,18 @@ const minutesOutput = document.querySelector('#minutes');
 const secondsOutput = document.querySelector('#seconds');
 
 function countdown() {
-  setInterval(() => {
-    let currentTime = new Date();
-    let distance = nextYear - currentTime;
+  let currentTime = new Date();
+  let distance = nextYear - currentTime;
 
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor(distance / (1000 * 60 * 60) % 24);
-    let minutes = Math.floor(distance / (1000 * 60) % 60);
-    let seconds = Math.floor(distance / 1000 % 60);
-    daysOutput.textContent = `${days}`;
-    hoursOutput.textContent = `${hours}`;
-    minutesOutput.textContent = `${minutes}`;
-    secondsOutput.textContent = `${seconds}`;
-  }, 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(distance / (1000 * 60 * 60) % 24);
+  let minutes = Math.floor(distance / (1000 * 60) % 60);
+  let seconds = Math.floor(distance / 1000 % 60);
+  daysOutput.textContent = `${days}`;
+  hoursOutput.textContent = `${hours}`;
+  minutesOutput.textContent = `${minutes}`;
+  secondsOutput.textContent = `${seconds}`;
 }
 
 countdown();
+setInterval(countdown, 1000);
